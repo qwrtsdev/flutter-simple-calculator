3e9f2ce9d6f7f007ea372fdf51063a791fe0fdd0
Pavich Komansil <phavit_k@hotmail.com>
2026-07-24 13:56:43 +0700

    separate pages into files.

---
+++ lib/about.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 12),
+            Text('Pavich Komansil 6801012610111'),
+            SizedBox(height: 12),
+            Text('Nuttapong Nakawirot 6801012610260'),
+            SizedBox(height: 12),
+            Text('Made By GPT Team (GamePavichTechnologia)'),
+          ],
+        ),
+      ),
+    );
+  }
+}

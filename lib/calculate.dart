1c04820b3b15e91912b3e4b7ccf698e25a74fae1
Pavich Komansil <phavit_k@hotmail.com>
2026-07-24 10:07:35 +0700

    Update and rename calculate_sum.dart to calculate.dart

---
+++ lib/calculate.dart
@@ -0,0 +1,22 @@
+double calculate(double num1, String operation, double num2) {
+    double answer = 0;
+
+    switch (operation) {
+      case '+':
+        answer = num1 + num2;
+        break;
+      case '-':
+        answer = num1 - num2;
+        break;
+      case '*':
+        answer = num1 * num2;
+        break;
+      case '/':
+        if (num2 == 0) {
+          answer = 0;
+        }
+        answer = num1 / num2;
+        break;
+    }
+    return answer;
+}
